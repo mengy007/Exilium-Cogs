56c9da2ff26811620065941921239c7e205d0357
Meng Yang <meng@mc3solutions.com>
2019-07-02 22:02:56 -0700

    test

--- exmboard/exmboard.py
+++ exmboard/exmboard.py
@@ -13,6 +13,8 @@ from PIL import Image, ImageDraw, ImageFont
 
 path = 'data/exilium/exmboard'
 
+bgImage = Image.open(path + '/bg.jpg').convert('RBGA')
+
 #bot = commands.Bot(command_prefix=commands.when_mentioned, description="Battlefield Stats Tracker")
 
 class ExmBoard:
@@ -157,13 +159,13 @@ class ExmBoard:
         await self.bot.send_typing(channel)
         try:
             # TEST
-            #txt = Image.new('RGBA', self.bgImage.size, (255,255,255,0))
-            #fnt = ImageFont.truetype(path + '/AnkaCoder-r.ttf', 100)
-            #d = ImageDraw.Draw(txt)
-            #d.text((10, 10), 'TEST', font=fnt, fill=(255,255,255,128))
-            #out = Image.alpha_composite(self.bgImage, txt)
-            #self.bot.send_file(ctx.message.channel, io.BytesIO(out.tobytes()), filename='exmboard.png')
-
+            txt = Image.new('RGBA', bgImage.size, (255,255,255,0))
+            fnt = ImageFont.truetype(path + '/AnkaCoder-r.ttf', 100)
+            d = ImageDraw.Draw(txt)
+            d.text((10, 10), 'TEST', font=fnt, fill=(255,255,255,128))
+            out = Image.alpha_composite(self.bgImage, txt)
+            self.bot.send_file(ctx.message.channel, io.BytesIO(out.tobytes()), filename='exmboard.png')
+            
 
             players = []
             for player in self.settings[server.id]['players']:
