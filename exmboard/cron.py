809f88e8577f2126537c0b6461a486ce19aa1d5f
Meng Yang <myang@nationalmerchants.com>
2019-07-03 13:03:23 -0700

    update to cron

--- exmboard/cron.py
+++ exmboard/cron.py
@@ -1,33 +1,40 @@
-import discord
-from discord.ext import commands
 import pathlib
 from cogs.utils.dataIO import dataIO
 import aiohttp
 import io
-from .utils import checks
 import json
 import operator
 import collections
-from PIL import Image, ImageDraw, ImageFont
 import urllib.request as urllib
+import asyncio
 
-path = 'data/exilium/exmboard/temp'
+path = 'data/exilium/exmboard'
 settings = dataIO.load_json(path + '/settings.json')
 playerData = []
-
-## main
-for server in settings:
-  print("Server: " + str(server) + "\n")
-  for player in settings[server]['players']:
-    print("Player: " + player + "\n")
-    await playerData.append(await fetch_stats(player))
-
-settings['playerData'] = playerData
-dataIO.save_json(path + '/settings.json', settings)
 
 ## functions
 async def fetch_stats(playername):
   url = "https://api.battlefieldtracker.com/api/v1/bfv/profile/origin/" + playername.replace(" ", "%20")
   print(" - URL: " + url + "\n")
-  async with aiohttp.get(url) as response:
-    return await response.json()
+  async with aiohttp.ClientSession() as session:
+    async with session.get(url) as response:
+      return await response.json()
+
+## main
+async def main():
+  try:
+    for server in settings:
+      print("Server: " + str(server) + "\n")
+      for player in settings[server]['players']:
+        print("Player: " + player + "\n")
+        playerData.append(await fetch_stats(player))
+
+      settings[server]['playerData'] = playerData
+
+    dataIO.save_json(path + '/settings.json', settings)
+
+  except Exception as e:
+    print('ERROR: ' + e)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
