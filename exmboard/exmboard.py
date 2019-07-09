1134c6ba4891a6a11c76f09114546509f78f1d34
Meng Yang <meng@mc3solutions.com>
2019-07-09 15:29:43 -0700

    fix

--- exmboard/exmboard.py
+++ exmboard/exmboard.py
@@ -294,8 +294,10 @@ class ExmBoard:
                     avatar = ''
                     try:
                       avatar = urllib.urlopen(player['avatarUrl'])
-                    except Exception as e:
+                    except Exception as e:                      
                       avatar = Image.new('RGB', (50, 50), '#000000')
+                      print('Blank avatar created for ' + player['name'])
+                      continue
                     avatarImageFile = io.BytesIO(avatar.read())                
                     avatarImage = Image.open(avatarImageFile).convert('RGB').resize((50, 50), Image.ANTIALIAS)
                     avatarCrop = avatarImage.crop((0, 0, 50, 50))
