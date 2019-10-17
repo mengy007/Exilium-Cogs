ce4366752a3fbe82ef8f90191e0bdc03bceaf5d5
Meng Yang <meng@mc3solutions.com>
2019-10-17 06:21:43 -0700

    test

--- exmboard/exmboard.py
+++ exmboard/exmboard.py
@@ -432,7 +432,7 @@ async def fetch_local_stats(self, ctx, player, scope, stat):
         if 'segments' in player and 'stats' in player['segments'][segment] and stat in player['segments'][segment]['stats'] and 'value' in player['segments'][segment]['stats'][stat]:
           value = player['segments'][segment]['stats'][stat]['value']
         else:
-          value = 'N/A'
+          value = 0
 
         print('Value: ' + str(value))
 
